6b1630fd11563389609ee05d7e2cc4eb01d15589
Micky <106265969+Omnipoz@users.noreply.github.com>
2024-05-02 07:36:18 +0000

    new file:   Python_Basics/basics_by_kushal/arithmetic_use.py

---
+++ Python_Basics/basics_by_kushal/arithmetic_use.py
@@ -0,0 +1,26 @@
+# Generally in python arithmetic follows general infix sturcture.
+
+# Addition
+print(3+3)
+
+# Subtraction
+print(2-1)
+
+# Division
+print(10/5) # This gives answer 2.which is an integer type.
+print(5/2) # This gives answer 2.5 which is a float type.
+print(5//2) # This // only give quitoint of the division.
+print(5%2) # This % is used for remainder.
+
+# Multiplication
+print(10*5) 
+print (2**3) # "**" means "left number" is raised to the power of "right number"
+
+# Simplification
+print((4-2)*50/2+3**3-1)
+# In python, simplification order is parenthesis > exponential > division or multiplication > addition or subtraction
+
+#Here div. and multiplication has same priority so what comes first from left side is operated.
+print(120/2*3) #ans is 180
+print(20*10/2) #ans is 100
+# Similar for addition and subtraction
