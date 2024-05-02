57c9faeba121977ea7a119fb7f6c4a703c57ba92
Micky <106265969+Omnipoz@users.noreply.github.com>
2024-05-02 08:01:18 +0000

    new file:   Python_Basics/basics_by_kushal/strings_in_py.py

---
+++ Python_Basics/basics_by_kushal/strings_in_py.py
@@ -0,0 +1,9 @@
+print ("Using double quote.") #everycharacter inside double quotes is a string value in python.
+print('Using single quote.') # We can use single quote too.
+#print('what's up')  
+# Due to cases like above double quotes are more preferred when single quote are in sentence to print and vice versa.
+print("What's up.")
+print('They said,"We are fine."') 
+
+#print('What's up')                 ERROR After "What" the single quote closes the initial single quote and the remaining is not considered string.
+#print("They said,"We are fine"")   ERROR  Same problem as above but due to double quote
